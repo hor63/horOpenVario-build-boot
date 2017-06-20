24e2d4db69da782d5085eec1143b798f31002458
Kai Horstmann <horstmannkai@hotmail.com>
2017-06-20 21:27:02 +0200

    Cleanup of boot scripts and fex files.
    Made the whole naming schema more systematic.

--- boot.hdmi.cmd
+++ boot.hdmi.cmd
@@ -1,6 +1,6 @@
 setenv bootm_boot_mode sec
-setenv bootargs console=tty0 root=/dev/mmcblk0p2 rootwait hdmi.audio=EDID:0 disp.screen0_output_mode=EDID:1920x1080p60 consoleblank=0
-ext2load mmc 0 0x43000000 script.bin.hdmi
+setenv bootargs console=tty0 root=/dev/mmcblk0p2 rootwait hdmi.audio=EDID:0 disp.screen0_output_mode=EDID:1920x1080p60 consoleblank=0 panic=10
+ext2load mmc 0 0x43000000 openvario.hdmi.bin
 ext2load mmc 0 0x55000000 uInitrdNoinst
 ext2load mmc 0 0x41000000 uImage
 bootm 0x41000000 0x55000000
--- boot.vga.cmd
+++ boot.vga.cmd
@@ -1,6 +1,6 @@
 setenv bootm_boot_mode sec
-setenv bootargs console=tty0 root=/dev/mmcblk0p2 rootwait hdmi.audio=EDID:0 disp.screen0_output_mode=EDID:1920x1080p-60 consoleblank=0
-ext2load mmc 0 0x43000000 script.bin.vga
+setenv bootargs console=tty0 root=/dev/mmcblk0p2 rootwait hdmi.audio=EDID:0 disp.screen0_output_mode=EDID:1920x1080p-60 consoleblank=0 panic=10
+ext2load mmc 0 0x43000000 openvario.vga.bin
 ext2load mmc 0 0x55000000 uInitrdNoinst
 ext2load mmc 0 0x41000000 uImage
 bootm 0x41000000 0x55000000
