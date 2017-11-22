e2366361bc1b087621e820a3d5c2fdeac5678179
Kai Horstmann <horstmannkai@hotmail.com>
2017-11-23 00:34:56 +0100

    Add rootwait to the kernel environment

--- boot.initrd-noinst.cmd
+++ boot.initrd-noinst.cmd
@@ -1,7 +1,7 @@
 # setenv bootm_boot_mode sec
 # setenv bootargs console=tty0 root=/dev/mmcblk0p2 rootwait consoleblank=0 panic=10
-setenv bootargs console=tty0 root=/dev/mmcblk0p2 consoleblank=0 panic=10
+setenv bootargs console=tty0 root=/dev/mmcblk0p2 rootwait consoleblank=0 panic=10
 ext2load mmc 0 0x43000000 sun7i-a20-cubieboard2.dtb
-ext2load mmc 0 0x45000000 uInitrdNoinst
+ext2load mmc 0 0x55000000 uInitrdNoinst
 ext2load mmc 0 0x41000000 zImage
-bootz 0x41000000 0x45000000 0x43000000 
+bootz 0x41000000 0x55000000 0x43000000 
