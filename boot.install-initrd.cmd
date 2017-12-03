31794e01ee76715decaf06957fdde7e1106ecc08
Kai Horstmann <horstmannkai@hotmail.com>
2017-12-03 21:47:50 +0100

    Adjust loading address for the initrd. Add boot.no-initrd.cmd

--- boot.install-initrd.cmd
+++ boot.install-initrd.cmd
@@ -1,6 +1,6 @@
 # setenv bootm_boot_mode sec
 setenv bootargs console=tty0 root=/dev/ram0 rootwait consoleblank=0 panic=10
 ext2load mmc 0 0x43000000 sun7i-a20-cubieboard2.dtb
-ext2load mmc 0 0x55000000 uMyinitrd
+ext2load mmc 0 0x44000000 uMyinitrd
 ext2load mmc 0 0x41000000 zImage
-bootz 0x41000000 0x55000000 0x43000000 
+bootz 0x41000000 0x44000000 0x43000000 
--- boot.no-initrd.cmd
+++ boot.no-initrd.cmd
@@ -1,6 +1,6 @@
 # setenv bootm_boot_mode sec
 setenv bootargs console=tty0 root=/dev/mmcblk0p2 rootwait consoleblank=0 panic=10
+
 ext2load mmc 0 0x43000000 sun7i-a20-cubieboard2.dtb
-ext2load mmc 0 0x44000000 uInitrdNoinst
 ext2load mmc 0 0x41000000 zImage
-bootz 0x41000000 0x44000000 0x43000000 
+bootz 0x41000000 - 0x43000000 
